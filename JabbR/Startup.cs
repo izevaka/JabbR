6e202f58e4ebaabc1cb6231d465ddfc9403774cf
Louis DeJardin <louis.dejardin@gmail.com>
2012-05-17 21:04:36 -0700

    Patching in the components that do request work
    Use(Sanity) provides early warning of incorrect call patterns
    UseHomePage calls a simplistic default.aspx renderer
    UseStatic returns filesystem objects for requests
    UseSignalRHubs dispatches requests to signalr
    RunHttpServer dispatches to WebAPI constrollers

--- JabbR/Startup.cs
+++ JabbR/Startup.cs
@@ -1,5 +1,8 @@
-﻿using System.IO;
-using System.Web.Hosting;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
 using System.Web.Http;
 using Gate.Adapters.AspNetWebApi;
 using Gate.Middleware;
@@ -16,19 +19,127 @@ namespace JabbR
             GlobalConfiguration.Configuration = new HttpConfiguration();
 
             Bootstrapper.PreAppStart();
-            
+
             builder
-                .UseStatic(BasePath())
-                .RunHttpServer(GlobalConfiguration.Configuration)
-                //.RunSignalR()
-                ;
+                .Use<AppDelegate>(Sanity)
+                .UseHomePage()
+                .UseStatic(TransitionalGlue.BasePath())
+                .UseSignalRHubs("/signalr", TransitionalGlue.Resolver)
+                .RunHttpServer(GlobalConfiguration.Configuration);
         }
 
-        private static string BasePath()
+        private AppDelegate Sanity(AppDelegate app)
         {
-            return HostingEnvironment.IsHosted 
-                ? HostingEnvironment.MapPath("~") 
-                : Directory.GetCurrentDirectory();
+            return
+                (env, result, fault) =>
+                {
+                    var watcher = new Watcher();
+                    watcher.Call(env);
+                    app(
+                        env,
+                        (status, headers, body) =>
+                        {
+                            watcher.Result(status, headers, body);
+                            result(
+                                status,
+                                headers,
+                                (write, flush, end, cancellationToken) =>
+                                {
+                                    watcher.Body(cancellationToken);
+                                    body(
+                                        write,
+                                        flush,
+                                        ex =>
+                                        {
+                                            watcher.End(ex);
+                                            end(ex);
+                                        },
+                                        cancellationToken);
+                                });
+                        },
+                        ex =>
+                        {
+                            watcher.Fault(ex);
+                            fault(ex);
+                        });
+                };
+        }
+
+        class Watcher
+        {
+            private Timer _timer;
+            private bool _resultCalled;
+            private bool _faultCalled;
+            private bool _bodyCalled;
+            private bool _endCalled;
+
+            private IDictionary<string, object> _env;
+            private string _status;
+            private IDictionary<string, IEnumerable<string>> _headers;
+            private BodyDelegate _body;
+            private Exception _faultException;
+            private Exception _endException;
+
+            public void Call(IDictionary<string, object> env)
+            {
+                _env = env;
+                _timer = new Timer(OnTimer, null, TimeSpan.FromSeconds(5), TimeSpan.FromMilliseconds(-1));
+            }
+
+            private void OnTimer(object state)
+            {
+                TraceAssert(_faultCalled || _resultCalled, "fault or result not called in time");
+                if (_resultCalled)
+                {
+                    TraceAssert(_bodyCalled, "body not called in time with result");
+                }
+                _timer.Dispose();
+            }
+
+            public void Result(string status, IDictionary<string, IEnumerable<string>> headers, BodyDelegate body)
+            {
+                TraceAssert(!_faultCalled, "result called after fault called");
+                TraceAssert(!_resultCalled, "result may only be called once");
+                _resultCalled = true;
+                _status = status;
+                _headers = headers;
+                _body = body;
+            }
+
+            public void Fault(Exception exception)
+            {
+                TraceAssert(!_resultCalled, "fault called after result called");
+                TraceAssert(!_faultCalled, "fault may only be called once");
+                _faultCalled = true;
+                _faultException = exception;
+            }
+
+            public void Body(CancellationToken cancellationToken)
+            {
+                TraceAssert(_resultCalled, "result must be called before response body");
+                TraceAssert(!_bodyCalled, "body may only be called once");
+                _bodyCalled = true;
+            }
+
+            public void End(Exception exception)
+            {
+                TraceAssert(_resultCalled, "result must be called before body end");
+                TraceAssert(_bodyCalled, "body must be called before body end");
+                TraceAssert(!_endCalled, "end may only be called once");
+                _endCalled = true;
+                _endException = exception;
+            }
+
+            private void TraceAssert(bool condition, string message)
+            {
+                if (condition) return;
+
+                object value;
+                if (_env.TryGetValue("host.TraceOutput", out value) && value is TextWriter)
+                {
+                    ((TextWriter)value).WriteLine(message);
+                }
+            }
         }
     }
 }
--- JabbR/TransitionalGlue.cs
+++ JabbR/TransitionalGlue.cs
@@ -1,20 +1,215 @@
 ﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Hosting;
 using System.Web.Http;
 using System.Web.Routing;
+using Owin;
 using SignalR.Ninject;
 
 namespace JabbR
 {
-    public static class GlobalConfiguration
+    public static class TransitionalGlue
     {
-        public static HttpConfiguration Configuration { get; set; }
+        public static string BasePath()
+        {
+            return HostingEnvironment.IsHosted
+                       ? HostingEnvironment.MapPath("~")
+                       : Directory.GetCurrentDirectory();
+        }
 
         public static void MapHubs(this RouteCollection routes, NinjectDependencyResolver resolver)
         {
+            Resolver = resolver;
         }
 
         public static void MapHttpRoute(this RouteCollection routes, string name, string routeTemplate)
         {
         }
+
+        public static IAppBuilder UseHomePage(this IAppBuilder builder)
+        {
+            var viewType = PseudoWebForms.Compile(Path.Combine(BasePath(), "default.aspx"));
+
+            return builder.Use<AppDelegate>(
+                app => (env, result, fault) =>
+                {
+                    var req = new Gate.Request(env);
+                    if (req.Path == "/")
+                    {
+                        var view = (PseudoWebForms.View)Activator.CreateInstance(viewType);
+                        view.Request = req;
+                        view.Response = new Gate.Response(result) { ContentType = "text/html" };
+                        view.RenderView();
+                        view.Response.End();
+                        return;
+                    }
+                    app(env, result, fault);
+                });
+        }
+
+
+        public static NinjectDependencyResolver Resolver { get; set; }
+    }
+
+    public static class GlobalConfiguration
+    {
+        public static HttpConfiguration Configuration { get; set; }
+
+    }
+
+    public static class PseudoWebForms
+    {
+        public abstract class View
+        {
+            public Gate.Request Request { get; set; }
+            public Gate.Response Response { get; set; }
+
+            public abstract void RenderView();
+
+            public void Write(string value)
+            {
+                Response.Write(value);
+            }
+            public void WriteLiteral(string value)
+            {
+                Write(value);
+            }
+            public void WriteEncoded(string value)
+            {
+                Write(HttpUtility.HtmlEncode(value));
+            }
+
+
+            public string ResolveClientUrl(string path)
+            {
+                if (path.StartsWith("~/", StringComparison.Ordinal))
+                {
+                    return Request.PathBase + path.Substring(1);
+                }
+                return path;
+            }
+        }
+
+        public static Type Compile(string filePath)
+        {
+            var defaultAspx = File.ReadAllText(filePath);
+
+            using (var writer = new StringWriter())
+            {
+                foreach (var segment in Segments(defaultAspx))
+                {
+                    switch (segment.Item1)
+                    {
+                        case "<%@":
+                            {
+                                var ns = segment.Item2.Trim();
+                                if (ns.StartsWith("Import namespace=\"", StringComparison.InvariantCultureIgnoreCase) &&
+                                    ns.EndsWith("\""))
+                                {
+                                    ns = ns.Substring("Import namespace=\"".Length,
+                                                      ns.Length - "Import namespace=\"".Length - 1);
+                                    writer.Write("using ");
+                                    writer.Write(ns);
+                                    writer.Write(";");
+                                }
+                                break;
+                            }
+                    }
+                }
+
+                writer.Write(@"
+using System;
+public class View : JabbR.PseudoWebForms.View
+{
+    public override void RenderView()
+    {
+");
+                foreach (var segment in Segments(defaultAspx))
+                {
+                    switch (segment.Item1)
+                    {
+                        case "literal":
+                            writer.Write("WriteLiteral(\"");
+                            writer.Write(segment.Item2
+                                             .Replace("\\", "\\\\")
+                                             .Replace("\"", "\\\"")
+                                             .Replace("\r", "\\r")
+                                             .Replace("\n", "\\n")
+                                             .Replace("\t", "\\t")
+                                );
+                            writer.WriteLine("\");");
+                            break;
+                        case "<%":
+                            writer.WriteLine(segment.Item2);
+                            break;
+                        case "<%=":
+                            writer.Write("Write(");
+                            writer.Write(segment.Item2);
+                            writer.WriteLine(");");
+                            break;
+                        case "<%:":
+                            writer.Write("WriteEncoded(");
+                            writer.WriteLine(segment.Item2);
+                            writer.WriteLine(");");
+                            break;
+                    }
+                }
+                writer.Write(@"
+    }
+}
+");
+
+                var source = writer.ToString();
+
+                var codeDomProvider = CodeDomProvider.CreateProvider("cs");
+
+                var forceToLoad = new[]
+                                      {
+                                          typeof (JabbR.GlobalConfiguration),
+                                          typeof (System.Configuration.ConfigurationManager),
+                                          typeof (SquishIt.Framework.Bundle),
+                                      };
+                var options = new CompilerParameters(
+                        AppDomain.CurrentDomain.GetAssemblies()
+                            .Where(x => !x.IsDynamic)
+                            .Select(x => x.Location)
+                            .ToArray());
+
+
+                var results = codeDomProvider.CompileAssemblyFromSource(options, source);
+                return results.CompiledAssembly.GetType("View");
+            }
+        }
+
+
+        static IEnumerable<Tuple<string, string>> Segments(string text)
+        {
+            var scan = 0;
+            while (scan != text.Length)
+            {
+                var sequences = new[] { "<%@", "<%:", "<%=", "<%" };
+
+                var next = sequences
+                    .Select(sequence => new { startIndex = text.IndexOf(sequence, scan, StringComparison.InvariantCulture), sequence })
+                    .Where(x => x.startIndex != -1)
+                    .OrderBy(x => x.startIndex)
+                    .Select(hit => new { startIndex = hit.startIndex, endIndex = text.IndexOf("%>", hit.startIndex, StringComparison.InvariantCulture), sequence = hit.sequence })
+                    .FirstOrDefault(x => x.endIndex != -1);
+
+                if (next == null)
+                {
+                    yield return Tuple.Create("literal", text.Substring(scan));
+                    break;
+                }
+                yield return Tuple.Create("literal", text.Substring(scan, next.startIndex - scan));
+                yield return Tuple.Create(next.sequence, text.Substring(next.startIndex + next.sequence.Length, next.endIndex - next.startIndex - next.sequence.Length));
+                scan = next.endIndex + 2;
+            }
+        }
+        
     }
 }
